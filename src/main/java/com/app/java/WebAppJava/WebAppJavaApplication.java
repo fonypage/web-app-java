67e64df12fc4e4cbba3d85063854b4a5db7d4b0b
FonyP <misha.ignatenko.2017@bk.ru>
2025-03-04 22:44:42 +0300

    dsf

---
+++ src/main/java/com/app/java/WebAppJava/WebAppJavaApplication.java
@@ -0,0 +1,13 @@
+package com.app.java.WebAppJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/java/WebAppJava/WebAppJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.java.WebAppJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
